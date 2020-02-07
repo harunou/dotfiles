cb335810ec83abfa909ae41b118b7bf6ce98310d
harunou <d.harunou@gmail.com>
2020-02-07 23:26:58 +0300

    Pretting xmonad.hs file

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,9 @@
 import XMonad
 
-main = xmonad def
-    { terminal = "urxvt"
-    , modMask = modm
---    , focusedBorderColor = "#000000"
+main = xmonad $ def
+  { terminal = "urxvt"
+  , modMask = modm
+  -- , focusedBorderColor = "#000000"
 
-    }
+  }
     where modm = mod4Mask
