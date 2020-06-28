d0bcb286519916e29ca8f8eb59dee4f99c9525a6
harunou <d.harunou@gmail.com>
2020-06-28 22:41:28 +0300

    Xbindkeys: add sound and mic hotkeys

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -1,4 +1,25 @@
 ;; ~/.xbindkeysrc.scm
 
+;; /usr/include/X11/keysymdef.h
+;; /usr/include/X11/XF86keysym.h
+
+(define mod
+  (string->symbol (or (getenv "XBINDKEYS_MOD") "mod4")))
+
 (xbindkey '(XF86MonBrightnessUp) "xbacklight -inc 10")
 (xbindkey '(XF86MonBrightnessDown) "[[ $(printf '%.0f\n' $(xbacklight)) -gt 11 ]] && xbacklight -dec 10")
+
+(xbindkey '(XF86AudioMute) "harunou media sink toggle")
+(xbindkey '(XF86AudioLowerVolume) "harunou media sink -5%")
+(xbindkey '(mod4 minus) "harunou media sink -5%")
+(xbindkey '(XF86AudioRaiseVolume) "harunou media sink +5%")
+(xbindkey '(mod4 plus) "harunou media sink +5%")
+(xbindkey '(mod4 shift equal) "harunou media sink 100%")
+
+(xbindkey '(mod1 XF86AudioMute) "harunou media source toggle")
+(xbindkey '(mod4 XF86AudioLowerVolume) "harunou media source -5%")
+(xbindkey '(mod1 minus) "harunou media source -5%")
+(xbindkey '(mod4 XF86AudioRaiseVolume) "harunou media source +5%")
+(xbindkey '(mod1 plus) "harunou media source +5%")
+(xbindkey '(mod1 shift equal) "harunou media source 100%")
+
