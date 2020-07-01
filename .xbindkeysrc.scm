08deb8de6038ebb39c2023a8ee3d9df85708af12
harunou <d.harunou@gmail.com>
2020-07-01 23:00:29 +0300

    Media: add media keybindings

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -7,7 +7,7 @@
   (string->symbol (or (getenv "XBINDKEYS_MOD") "mod4")))
 
 (xbindkey '(XF86MonBrightnessUp) "xbacklight -inc 10")
-(xbindkey '(XF86MonBrightnessDown) "[[ $(printf '%.0f\n' $(xbacklight)) -gt 11 ]] && xbacklight -dec 10")
+(xbindkey '(XF86MonBrightnessDown) "[[ $(printf '%.0f\n' $(xbacklight)) -gt 20 ]] && xbacklight -dec 10")
 
 (xbindkey '(XF86AudioMute) "harunou media sink toggle")
 (xbindkey '(XF86AudioLowerVolume) "harunou media sink -5%")
@@ -16,10 +16,11 @@
 (xbindkey '(mod4 plus) "harunou media sink +5%")
 (xbindkey '(mod4 shift equal) "harunou media sink 100%")
 
+(xbindkey '(XF86AudioMicMute) "harunou media source toggle")
 (xbindkey '(mod1 XF86AudioMute) "harunou media source toggle")
-(xbindkey '(mod4 XF86AudioLowerVolume) "harunou media source -5%")
+(xbindkey '(mod1 XF86AudioLowerVolume) "harunou media source -5%")
 (xbindkey '(mod1 minus) "harunou media source -5%")
-(xbindkey '(mod4 XF86AudioRaiseVolume) "harunou media source +5%")
+(xbindkey '(mod1 XF86AudioRaiseVolume) "harunou media source +5%")
 (xbindkey '(mod1 plus) "harunou media source +5%")
 (xbindkey '(mod1 shift equal) "harunou media source 100%")
 
