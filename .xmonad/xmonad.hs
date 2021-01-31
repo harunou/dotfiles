370a36a47945f256b78f2af3e4bfe8ca19a046d2
harunou <d.harunou@gmail.com>
2021-01-31 18:24:11 +0100

    Xmonad: make full screen borderless

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,18 @@
+import System.Exit
 import XMonad
+import XMonad.Config.Desktop
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.BinarySpacePartition (emptyBSP)
+import XMonad.Layout.NoBorders (noBorders)
+import XMonad.Layout.ResizableTile (ResizableTall(..))
+import XMonad.Layout.ToggleLayouts (ToggleLayout(..), toggleLayouts)
 
-main = xmonad $ def
+main = xmonad $ desktopConfig
   { terminal = "urxvt"
   , modMask = modm
-  -- , focusedBorderColor = "#000000"
-
+  , focusFollowsMouse = False
+  , layoutHook = desktopLayoutModifiers $ defaultTall ||| noBorders (Full) ||| Mirror (defaultTall)
   }
-    where modm = mod4Mask
+    where modm = mod4Mask,
+          defaultTall = Tall 1 3/100 1/2
