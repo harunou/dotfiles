cff2b1aa1d37e666e2c34d0b72a8e2a8954d30b5
harunou <d.harunou@gmail.com>
2021-02-16 19:21:16 +0100

    Xmonad: make fullscreen cover all display

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,7 +8,7 @@ import XMonad.Layout
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.NoBorders
 
-myLayout = desktopLayoutModifiers $ tallLayout ||| wideLayout ||| fullLayout
+myLayout = tallLayout ||| wideLayout ||| fullLayout
   where
     basicLayout = smartBorders $ fullscreenFocus $ Tall 1 (3/100) (1/2)
     tallLayout  = avoidStruts $ basicLayout
@@ -18,13 +18,13 @@ myLayout = desktopLayoutModifiers $ tallLayout ||| wideLayout ||| fullLayout
 main = do 
   spawn "xmobar"
 
-  xmonad $ ewmh $ desktopConfig
+  xmonad $ fullscreenSupport $ ewmh $ desktopConfig
     { terminal = "urxvt"
     , modMask = modm
     , focusFollowsMouse = False
     , normalBorderColor = "#37474f"
     , focusedBorderColor = "#06989A"
-    , layoutHook = myLayout
+    , layoutHook = desktopLayoutModifiers myLayout
     }
       where 
         modm = mod4Mask
