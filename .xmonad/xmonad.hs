cbbb5ba9898542da32bc4086d8ce60708c61fae7
harunou <d.harunou@gmail.com>
2022-06-10 12:32:52 +0200

    Xmonad: add a pinch of shortcuts

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -13,6 +13,8 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.ToggleLayouts
 import XMonad.Layout.Renamed
 import XMonad.Util.EZConfig
+import XMonad.Actions.CycleWS
+import qualified XMonad.StackSet as W
 
 layouts = toggle $ tallLayout ||| wideLayout ||| fullLayout
   where
@@ -38,19 +40,31 @@ main = do
     . ewmh 
     . withUrgencyHook NoUrgencyHook 
     $ desktopConfig
-      { terminal = "urxvt"
+      { terminal = term
       , modMask = modm
       , focusFollowsMouse = False
       , normalBorderColor = "#37474f"
       , focusedBorderColor = "#06989A"
       , layoutHook = desktopLayoutModifiers layouts
       , manageHook = manageHooks
+      , workspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
       , logHook = dynamicLogString def {
           ppUrgent = xmobarColor "red" "" . wrap "!" "!"
-        } >>= xmonadPropLog
+                                       } >>= xmonadPropLog
       } 
       `additionalKeys`
       [ ((modm              , xK_f), sendMessage (Toggle "Full"))
       , ((modm .|. shiftMask, xK_f), sendMessage ToggleStruts) 
+      , ((modm,               xK_BackSpace), spawn term)
+      , ((modm,               xK_bracketright), moveTo Next NonEmptyWS)
+      , ((modm,               xK_bracketleft), moveTo Prev NonEmptyWS)
+      , ((modm,               xK_backslash), toggleWS)
+      , ((modm,               xK_y), windows $ W.greedyView "7")
+      , ((modm,               xK_u), windows $ W.greedyView "8")
+      , ((modm,               xK_i), windows $ W.greedyView "9")
+      , ((modm,               xK_o), windows $ W.greedyView "0")
+      , ((modm,               xK_0), windows $ W.greedyView "0")
+      , ((modm .|. shiftMask, xK_0), windows $ W.shift "0")
       ] 
         where modm = mod4Mask
+              term = "urxvt"
